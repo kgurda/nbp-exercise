5acc0ad65a78b460443134039e6f7f2eefe8406a
Dominik <dburdzy@gmail.com>
2018-05-08 22:03:38 +0200

    Downloading list of urls.

--- src/main/java/com/kasia/master/nbp/MainClass.java
+++ src/main/java/com/kasia/master/nbp/MainClass.java
@@ -11,11 +11,6 @@ public class MainClass {
     private static NbpCurrencyApi api = new NbpCurrencyApi();
 
     public static void main(String[] args) {
-        try {
-            api.getAverageCurrency(Currency.USD, new Date(1), new Date(1));
-        } catch(NotImplementedException ex) {
-            ex.printStackTrace();
-        }
         System.out.println("aaaa");
     }
 }
--- src/main/java/com/kasia/master/nbp/api/NbpCurrencyApi.java
+++ src/main/java/com/kasia/master/nbp/api/NbpCurrencyApi.java
@@ -5,16 +5,20 @@ import com.kasia.master.nbp.exception.NotImplementedException;
 import com.kasia.master.nbp.model.Currency;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
 public class NbpCurrencyApi {
 
-    public static final String URL = "http://www.nbp.pl/kursy/xml/c";
-    public static final String CONTENT_TYPE = "application/xml";
+    private static final String LIST_URL = "http://www.nbp.pl/kursy/xml/dir.txt";
+    private static final String XML_CONTENT_TYPE = "application/xml";
+    private static final String TEXT_CONTENT_TYPE = "text/plain";
 
     public BigDecimal getAverageCurrency(Currency currency, Date startDate, Date endDate) throws NotImplementedException {
 
-        String response = HttpProvider.executeGet(URL, CONTENT_TYPE);
+        String response = HttpProvider.executeGet(LIST_URL, TEXT_CONTENT_TYPE);
+        List<String> urls = Arrays.asList(response.split("\n"));
 
         throw new NotImplementedException("Jeszcze nie ma!");
     }
